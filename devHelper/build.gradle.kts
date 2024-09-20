b408da3cff6adc134937c1eb23d2a980357be1fc
陈国浈 <1021632321@qq.com>
2024-09-20 12:11:24 +0800

    add info plugins

--- devHelper/build.gradle.kts
+++ devHelper/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(11))
+            languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
 }
